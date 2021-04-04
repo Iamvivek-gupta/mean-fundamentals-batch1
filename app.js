4ed5fcb07ec777ea8a3223d36854184338d4e3d6
VIVEK GUPTA <vivek.jmdsmvdu@gmail.com>
2021-04-04 20:46:36 +0530

    rules for variables

---
+++ app.js
@@ -0,0 +1,7 @@
+// Rulse to declaration variables
+// i) variables contain letter number underscore and dollor symbol
+// ii) can not start with number
+// iii) can't use let var const keyword
+
+let name = "codex";
+console.log(name);
