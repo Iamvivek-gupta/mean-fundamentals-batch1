f5ff58b563632462e10b0e20ad542fabea3ec198
VIVEK GUPTA <vivek.jmdsmvdu@gmail.com>
2021-04-07 20:32:59 +0530

    conditions

--- app.js
+++ app.js
@@ -57,5 +57,64 @@
 // var y = 2*(2+2)
 // console.log(x, y);
 
-var name = ["komal",true, 2, null, undefined];
-console.log(typeof name);
+// var name = ["komal",true, 2, null, undefined];
+// console.log(typeof name);
+// Arithmatic Operator
+
+
+//console.log(20 <= 20);
+
+// condition
+
+
+//1. if statement
+
+        // if(expression){
+        //    content to be executed
+        //   }
+
+
+if(-5 < -6){
+    console.log("hallo vijay");
+}    
+
+// 2. if else statement
+// syntax
+// if(expression){
+//     content to be execute
+// } else {
+//     content to be executed
+// }
+// if(-5 < -6){
+//     console.log("hallo vijay");
+// } else {
+//     console.log("halo Codex");
+// }
+
+
+// 3. else if statement
+
+// if(expression){
+//     content to be executed
+// } else if(expression){
+//     content to be executed
+// } else {
+//     content to be executed
+// }
+
+// var a = 10;
+// var b = 20;
+// if(b > b){
+//     console.log("hallo Komal");
+// } else if( b < a) {
+//   console.log("hallo vijay");
+// } else{
+//     console.log("hallo vedant");
+// }
+// 2,4,6,8,10
+var a = 4;
+if (a%2===0){
+    console.log("given number is even " + a);
+} else {
+    console.log("given number is odd " + a);
+}
