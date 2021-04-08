e1e0baf35944df3109e31396b22460f141297536
komal <kawaghmare374@gmail.com>
2021-04-08 20:25:25 +0530

    changes

--- app.js
+++ app.js
@@ -15,3 +15,66 @@ console.log(name);
 // git branch -M main
 // git remote add origin https://github.com/Iamvivek-gupta/mean-fundamentals-batch1.git
 // git push -u origin main
+//
+
+
+// condition
+//java scrip if else statement is used to execute code wether condition is true or false
+
+//     1. if statement
+
+//      if(expression)
+//     {
+ //   content to be executed
+///    }
+
+//if (-5 >-6){
+    //console.log("hello vijay");
+//}
+
+/// if Else statement
+
+// if(expression)
+//{
+ //   content to be executed
+//}else
+//{
+    //content to be executed
+//}
+
+if (-5 < -6)
+{
+    console.log("hello vijay");
+}
+else{
+    console.log("hello  codex");
+}
+
+//   3. else if statement
+//if (expression){
+   // conetct to be executed
+//}else if(expression)
+//{
+  //  contect to be executed
+
+//}
+
+//else {
+  //  content to be executed
+//}
+
+//% remainder and /n
+ ///2,4,6,8,10 
+
+//evan and odd 
+//var a=3 
+//node
+
+
+
+
+
+
+
+
+
