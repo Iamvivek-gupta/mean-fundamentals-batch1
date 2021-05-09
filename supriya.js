512ca002db20d910284a1a62bf54a37d962fdde6
VIVEK GUPTA <vivek.jmdsmvdu@gmail.com>
2021-05-09 13:24:01 +0530

    operators for loop

--- supriya.js
+++ supriya.js
@@ -8,7 +8,7 @@
 
 // arithmtic operator in javascript
 
-var a = 10;
+// var a = 10;
 // var b = "hi";
 // var result = a - b;
 // console.log(result);
@@ -33,6 +33,67 @@ var a = 10;
 // console.log("original array", a);
 // console.log(a.push(11,12,13,14,15));
 
-var fruits = ["banana", "mongo", "papaya"];
-console.log(fruits.indexOf(1));
+// var fruits = ["banana", "mongo", "papaya"];
+// var animal = ["dog", "pickok", "bird"];
+// var species = fruits.concat(animal);
+// console.log(species);
 
+
+
+// increment and decrement operator
+// var a = 3;
+// console.log(a--);
+// console.log(a);
+
+
+// if else condtion
+
+// if(2 >= 3){
+//     console.log("hallo supriya");
+// } else if (3 <1) {
+//     console.log("hallo vijay");
+// } else{
+//     console.log("welcome komal");
+// }
+
+/*
+if(condition){
+    code for execution
+} else {
+    code for execution
+}
+*/
+
+// ternory operator in js
+//2 < 1 ? console.log("halo komal") : console.log("hallo vijay");;
+//condition ? if condition true : then this will execute
+
+
+//var nums = [1,2,3,4,5,6,7,8,9];
+
+//for loop
+// let evenNumber = [];
+// for(var i = 1; i <= 20; i++){
+//     if(i % 2 == 0 ){
+//         evenNumber.push(i)
+//     }
+// }
+
+// console.log(evenNumber);
+// let a = []
+// for(var i = 1; i <= 20; ++i){
+//     if(i > 5){
+//         a.push(i)
+//     }
+// }
+
+// console.log(a);
+
+// [1,2,3,4,5,6]
+// [6,5,4,3,2,1]
+// let original = [7,8,3,4,5,6];
+// let reversre = [];
+// for(let i = original.length-1; i >= 0; i--){
+//     reversre.push(original[i])
+// }
+// console.log(reversre);
