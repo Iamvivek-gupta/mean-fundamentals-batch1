7339e5a943811b7fcc835ab82742de661469f044
komal <kawaghmare374@gmail.com>
2021-05-14 17:09:15 +0530

    practices

--- task.js
+++ task.js
@@ -12,51 +12,14 @@
 
 
 /***************** UpperCase *******************/
-// let word = "lotus is flower"; 
-// console.log(word);
-// let a=[];
-// result = word.split(" ");
-// console.log(result);
-// for(i=0;i<result;i++)
-// {
-//     letter=result.charAt(0);
-
-//     a.push(letter)}
-
-//     Uppercase= a.toUpperCase() +a.slice(1);
-
-// console.log(Uppercase);
-// {
-// Uppercase= letter.toUpperCase() +letter.slice(1);{
-// a.push(UpperCase)
-// }
-//     }
-
-// console.log(a);
+let word = "lotus is flower"; 
+console.log(word);
+result = word.split(" ");
+console.log(result);
+for (var i = 0; i < result.length; i++) {
+  result[i] = result[i].charAt(0).toUpperCase() + result[i].slice(1); 
+}
+console.log(result);
 
 
-//  {
-//    for(i=0;i<result.length;i++){
-//  UpperCase= result[i].toUpperCase();
-//           a.push(UpperCase)
-// }
-// console.log(UpperCase);
-// console.log(a);
-
-//let result = word.split(" ");
-//for(let i=0;i<word.indexOf();i++)
-  //  console.log(word);
-
-
-/*let str : "lotus";
-let textformat = (str
-    str[0].toUpperCase() + str.slice(1); 
-} 
- console.log(str);*/
-
- /*let array = [2,3,4,5,67,89]
- function removeElement(arr, value){
-     let start = arr.indexOf(value);
-     arr.splice(start, 2);
-     console.log(arr);*/
-    
+  
